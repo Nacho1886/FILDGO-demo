94d1c0921cdb252a67e4b24660fed94e85119c43
Nacho1886 <francert@hotmail.com>
2023-03-09 04:00:09 +0100

    Data and images updated

---
+++ src/app/fildgo/interfaces/product-interface.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    "id": number,
+    "name": string,
+    "name_description": string,
+    "place": string,
+    "place_description": string,
+    "start_seasson": string,
+    "finish_seasson": string
+}
